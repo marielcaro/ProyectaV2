812fa1ff130e1168cc7136f25fbae0422cf0f0c5
marielcaro <marielcaro8@gmail.com>
2023-03-16 20:40:35 -0300

    dashboard sidebar corrections

--- proyecta-v2/src/pages/Dashboard/dashboardSideBar/dashboardSideBar.jsx
+++ proyecta-v2/src/pages/Dashboard/dashboardSideBar/dashboardSideBar.jsx
@@ -15,20 +15,38 @@ import { useSelector, useDispatch } from 'react-redux'
 import { projects, dashboard, tasks, calendar } from '../../../features/dashboard/dashboardAction.js'
 
 const DashboardSideBar = () => {
-const [dashImage, setDashImage] = useState(dashboardnormal);
+const [dashImage, setDashImage] = useState(dashboardIcon);
+const [selected, setSelected] = useState("dashboard");
+const [menuDashboardItemStyle, setMenuDashboardItemStyle] = useState({backgroundColor: '#FFFFFF',  color: '#EB5401'})
+const [labelDashboardItemStyle, setLabelDashboardItemStyle] = useState({fontWeight: 'normal'})
+
+
 const [projectsImage, setProjectsImage] = useState(proyectos);
+const [menuProjectsItemStyle, setMenuProjectsItemStyle] = useState()
+const [labelProjectsItemStyle, setLabelProjectsItemStyle] = useState()
+
+
 const [tasksImage, setTasksImage] = useState(tareas);
+const [menuTaskItemStyle, setMenuTaskItemStyle] = useState()
+const [labelTaskItemStyle, setLabelTaskItemStyle] = useState()
+
 const [calendarImage, setCalendarImage] = useState(calendario);
+const [menuCalendarItemStyle, setMenuCalendarItemStyle] = useState()
+const [labelCalendarItemStyle, setLabelCalendarItemStyle] = useState()
+
 
 
 const dispatch = useDispatch()
 
 const handleHoverDashboard = () =>{
+  
   setDashImage(dashboardIcon)
 }
 
 const handleLostFocusDashboard = () =>{
+  if( selected !== "dashboard"){
   setDashImage(dashboardnormal)
+  }
 }
 
 const handleHoverProyectos = () =>{
@@ -36,66 +54,165 @@ const handleHoverProyectos = () =>{
 }
 
 const handleLostFocusProyectos = () =>{
+  if( selected !== "proyectos"){
   setProjectsImage(proyectos)
+  }
 }
 
 
 const handleHoverTareas = () =>{
+ 
   setTasksImage(tareasFocus)
+  
 }
 
 const handleLostFocusTareas = () =>{
+  if( selected !== "tareas"){
   setTasksImage(tareas)
+  }
 }
 
 
 const handleHoverCalendar = () =>{
+ 
   setCalendarImage(calendarioFocus)
+  
 }
 
 const handleLostFocusCalendar = () =>{
+  if( selected !== "calendario"){
   setCalendarImage(calendario)
+  }
 }
 
+const handlerClickItem  = (item) => {
+  switch (item) {
+    case "dashboard":
+      dispatch(dashboard())
+      setMenuDashboardItemStyle({backgroundColor: '#FFFFFF',  color: '#EB5401'})
+      setDashImage(dashboardIcon)
+      setLabelDashboardItemStyle({fontWeight: 'bold'})
+      setSelected("dashboard")
+
+      setMenuProjectsItemStyle()
+      setProjectsImage(proyectos)
+      setLabelProjectsItemStyle()
+
+      setMenuTaskItemStyle()
+      setTasksImage(tareas)
+      setLabelTaskItemStyle()
+
+      setMenuCalendarItemStyle()
+      setCalendarImage(calendario)
+      setLabelCalendarItemStyle()
+      break;
+  
+      case "proyectos":
+        dispatch(projects())
+        setMenuProjectsItemStyle({backgroundColor: '#FFFFFF',  color: '#EB5401'})
+        setProjectsImage(proyectosFocus)
+        setLabelProjectsItemStyle({fontWeight: 'bold'})
+        setSelected("proyectos")
+
+        setMenuDashboardItemStyle()
+        setDashImage(dashboardnormal)
+        setLabelDashboardItemStyle()
+
+        setMenuTaskItemStyle()
+        setTasksImage(tareas)
+        setLabelTaskItemStyle()
+  
+        setMenuCalendarItemStyle()
+        setCalendarImage(calendario)
+        setLabelCalendarItemStyle()
+
+
+        break;
+  
+        case "tareas":
+          dispatch(tasks())
+          setMenuTaskItemStyle({backgroundColor: '#FFFFFF',  color: '#EB5401'})
+          setTasksImage(tareasFocus)
+          setLabelTaskItemStyle({fontWeight: 'bold'})
+          setSelected("tareas")
+
+          setMenuDashboardItemStyle()
+        setDashImage(dashboardnormal)
+        setLabelDashboardItemStyle()
+
+              setMenuProjectsItemStyle()
+      setProjectsImage(proyectos)
+      setLabelProjectsItemStyle()
+
+        setMenuCalendarItemStyle()
+        setCalendarImage(calendario)
+        setLabelCalendarItemStyle()
+        break;
+
+          case "calendario":
+            dispatch(calendar())
+            setMenuCalendarItemStyle({backgroundColor: '#FFFFFF',  color: '#EB5401'})
+            setCalendarImage(calendarioFocus)
+            setLabelCalendarItemStyle({fontWeight: 'bold'})
+            setSelected("calendario")
+
+            setMenuDashboardItemStyle()
+            setDashImage(dashboardnormal)
+            setLabelDashboardItemStyle()
+
+            setMenuProjectsItemStyle()
+      setProjectsImage(proyectos)
+      setLabelProjectsItemStyle()
+    
+            setMenuTaskItemStyle()
+            setTasksImage(tareas)
+            setLabelTaskItemStyle()
+            break;
+        
+    default:
+      break;
+  }
+
+}
 
 
     return(
 <div className='sidebarContainer'style={{ display: 'flex', height: '100%' }}>
     <Sidebar  className= 'sidebar' breakPoint="lg"  backgroundColor= '#F6EBF9'>
     <Menu>
-      <MenuItem className='menuItem' label="tablero" onClick={() => dispatch(dashboard())} onMouseOver={() => handleHoverDashboard()} onMouseOut={() => handleLostFocusDashboard()}>
+      <MenuItem className='menuItem' style={menuDashboardItemStyle} label="tablero" onClick={() => handlerClickItem("dashboard")} onMouseOver={() => handleHoverDashboard()} onMouseOut={() => handleLostFocusDashboard()}>
                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                                         <img className='icons' src={dashImage} height="24" width="24" alt="tablero" />
-                                        <label> 
+                                        <label style={labelDashboardItemStyle} > 
                                           TABLERO
                                         </label>
 
                                     </Box>
     
       </MenuItem>
-      <MenuItem className='menuItem' label="proyectos" onClick={() => dispatch(projects())} onMouseOver={() => handleHoverProyectos()} onMouseOut={() => handleLostFocusProyectos()}> 
+      <MenuItem className='menuItem' style={menuProjectsItemStyle} label="proyectos" onClick={() => handlerClickItem("proyectos")} onMouseOver={() => handleHoverProyectos()} onMouseOut={() => handleLostFocusProyectos()}> 
        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                                         <img className='icons' src={projectsImage} height="24" width="24" alt="tablero" />
-                                        <label> 
+                                        <label  style={labelProjectsItemStyle}> 
                                           PROYECTOS
                                         </label>
 
                                     </Box>
      </MenuItem>
-      <MenuItem className='menuItem' label="tareas" onClick={() => dispatch(tasks())} onMouseOver={() => handleHoverTareas()} onMouseOut={() => handleLostFocusTareas()} >  
+      <MenuItem className='menuItem' style={menuTaskItemStyle} label="tareas" onClick={() => handlerClickItem("tareas")} onMouseOver={() => handleHoverTareas()} onMouseOut={() => handleLostFocusTareas()} >  
       <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                                         <img className='icons' src={tasksImage} height="24" width="24" alt="tablero" />
-                                        <label> 
+                                        <label  style={labelTaskItemStyle}> 
                                           TAREAS
                                         </label>
 
                                     </Box>
      </MenuItem>
 
-     <MenuItem className='menuItem' label="calendario" onClick={() => dispatch(calendar())} onMouseOver={() => handleHoverCalendar()} onMouseOut={() => handleLostFocusCalendar()}>  
+     <MenuItem className='menuItem'  style={menuCalendarItemStyle} label="calendario" onClick={() => handlerClickItem("calendario")} onMouseOver={() => handleHoverCalendar()} onMouseOut={() => handleLostFocusCalendar()}>  
      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                                         <img className='icons' src={calendarImage} height="24" width="24" alt="tablero" />
-                                        <label> 
+                                        <label  style={labelCalendarItemStyle}> 
                                           CALENDARIO
                                         </label>
 
