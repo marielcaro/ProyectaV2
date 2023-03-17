75baa76278b6946739737dd771f6e4831f61b8fc
marielcaro <marielcaro8@gmail.com>
2023-03-17 20:56:17 -0300

    begins of the task page

---
+++ proyecta-v2/src/pages/Dashboard/TasksContainerPage/Column.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+const Column = ({isOver,children})=>{
+    const className = isOver ? "Highlight-region" : ""
+    return (
+      <div className={`col${className}`}></div>,
+        {children}
+    );
+};
+export default Column;
