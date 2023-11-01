27af80de1b1805f4f5b284554cd63caa483cfa4b
marielcaro <marielcaro8@gmail.com>
2023-10-31 21:24:28 -0300

    refactor

--- proyecta-v2/src/App.js
+++ proyecta-v2/src/App.js
@@ -6,41 +6,34 @@ import LoginAlone from './pages/LoginAlone';
 
 import Dashboard from './pages/Dashboard/dashboard';
 
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const login = useSelector((state) => state.login.value)
 
-  const dispatch = useDispatch()
-
   const Page  = () => {
     switch (login) {
       case 'Login':
         return   (<LoginAlone />)
-      break;
       case 'Access':
         return   (<Dashboard />)
-      break;
+      
       case 'Exit':
         return (<MainPage />)
-        break;
      
       case 'Initial':
         return (<MainPage />)
-        break;
 
          case 'Register':
         return (<RegisterUserPage />)
-        break;
+        
 
         case 'Create':
           return (<MainPage />)
-          break;
         case 'Recover':
           return (<MainPage />)
-        break;
 
   };
 
